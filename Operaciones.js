1e7c2c92a4baaa95ad230d9a704437f5acf802eb
Juliana Castillo <96964513+jlianacastillo@users.noreply.github.com>
2022-02-16 18:56:58 -0500

    Create Operadores de Asignacion

---
+++ Operaciones.js
@@ -0,0 +1,34 @@
+//En estas operaciones no hay ejemplo de debilmente tipado
+
+3 + 2
+50 - 10
+10 * 20
+20 / 2
+
+//Concatenacion de un solo valor con dos valores
+
+"Juliana " + "Castillo"
+
+! //caracter de negacion
+
+!false 
+
+//en consola sale true
+
+//Operacion de Asignacion 
+
+var a = 1;
+
+//Inicializar es asignar un valor
+
+//Valores de Comparacion
+
+== // Es comparar
+3 == "3"; 
+
+//true
+
+=== // Es validar
+3 === "3"; 
+
+//false
